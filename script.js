aef726f0ef63166627a48ddacd7b59093eca8860
jaimie <moore.jaimie@gmail.com>
2019-02-15 12:30:52 -0500

    partially namespaced and functional

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
+//step 1: create global variable for user's input to go later, when we will print to the screen 
+//step 3a: if user enters nothing - print "you must neter a question"
+//step 3b: on click, print user input
+//step 4: on click, print random answer
+const myApp = {};
+myApp.$userQuestion = '';
+myApp.$fortune = $('.fortune'); //defining variables for html class
+myApp.$question = $('.user-question'); //defining variables for html class
 
 //step 2: create an array of standard answers
-const answers = [
+myApp.answers = [
     "As I see it, yes",
     "Ask again later",
     "Better not tell you now",
@@ -24,67 +32,59 @@ const answers = [
     "You may rely on it"
 ];
 
-//stretch 1 - second array - funny answers
-const alternativeAnswers = [
-    "yes, but do it drunk AF",
-    "my sources say no, but they also said Hilary would win",
-    "don't swipe right it's your cousin",
-    "that's an interesting question, a more interesting question: how is applebees still in business? wild.",
-    "you have a therapist for this",
-    "I'm not responsible for the consequences of this, but yes.",
-    "heck you betcha!",
-    "you should hire a lawyer",
-    "does a bear shit in the woods?",
-    "I am God's vessel - Kanye West",
-    "When you're the absolute best, you get hated the most",
-    "I will go down as the voice of this generation, of this decade, I will be the loudest voice",
-    "you should only believe about 90% of what I say, as a matter of fact, dont' even believe anything I'm saying at all. I could be completeing [effing] with you, and the world, the entire time"
-];
+//event listener for submit button
+    myApp.handleSubmit = function(){
+        $('.form-submit').on('submit', function (e) {
+            e.preventDefault();
 
-//step 2: on click prevent default refresh
+            //method on submit to randomize answers.
+            myApp.randoNum = () => {
+                // console.log(Math.floor(Math.random() * myApp.answers.length));
+                
+                return Math.floor(Math.random() * myApp.answers.length);
+                
+            };  //randoNum method ends here
 
+            
+            //conditional to determine if user left input box blank
+            myApp.userInputValue = function() {
+                if ($('.form-submit input[type=text]').val() !== '') {
+                    //$questionInput is equal to the value of the user input
+                    myApp.$questionInput = $('input[type=text]').val();
+                    //the empty $userQuestion variable defined at the top is equal to $questionInput
+                    myApp.$userQuestion = myApp.$questionInput
+                };
+            }; //userInput ends here
+            
+            
+            myApp.printToPage = function (item) {
+                //variable to store value of myApp.randoNum()
+                let random = myApp.randoNum();
+                //print to page, the user's question & Magic 8 Ball Fortune
+                myApp.$question.html(`<h2 class="print-question">You typed ${myApp.$userQuestion} ?</h2>`);
+                myApp.$fortune.html(`<h2 class="answer">Magic 8 ball says: ${myApp.answers[random]}</h2>`);
+            };
+            
+            myApp.randoNum(); // calling randoNum Function
+            myApp.userInputValue(); //calling userInputValue Function
+            myApp.printToPage(); // calling printToPage Function
+        });
+    }
 
-//step 3a: if user enters nothing - print "you must neter a question"
-//step 3b: on click, print user input
-
-//step 4: on click, print random answer
-
-
-
-//step 1: create global variable for user's input to go later, when we will print to the screen 
-let userQuestion = '';
-
-$(function () {
-    $('.form-submit').on('submit', function (e) {
-        e.preventDefault();
-    
-        // let userInput = $('input[name=question]').val();
-
-    //    //check to determine if user left input box blank
-        if ($('.form-submit input[type=text]').val() !== ''){
-            const question = $('input[type=text]').val();
-            //print users input to screen
-            $('.user-question').html(`<h2 class="print-question">You typed ${question} ?</h2>`);
-        };
-    //     }  else {
-    //         alert('enter a question');
-    //     }
-
-       
-
-        //randomize answers array
-        const randoNum = Math.floor(Math.random() * answers.length);
-        //print answers array value to DOM
-        $('.fortune').html(`<h2 class="answer">Magic 8 ball says: ${answers[randoNum]}</h2>`);
-    });
-
-    //clear fortune, and clear users input so it's clean and they can enter something else into the input field. 
-    const clearInput = function () {
+//clear fortune, and clear users printed input so they can enter something else into the input field. 
+    myApp.clearInput = function () {
         $('.re-submit').on('click', function () {
             $('.user-input').val('');
             $('.fortune').empty('')
-            });
-        } 
-        clearInput(); //call function
+        });
+    }; 
     
+//initializing everything on page load
+    myApp.init = function(){
+        myApp.handleSubmit(); //calling handleSubmit Function
+        myApp.clearInput(); // calling clearInput Function
+    };
+
+$(function(){
+    myApp.init();
 }) //doc ready
